385fbbcf7eea4d7077442a275a03c30b8b427096
ilmi Azmi <64541655+ilmi19@users.noreply.github.com>
2020-10-27 14:00:42 +0700

    praktikum 6

---
+++ E41192330_Ilmi_Azmi_Iradiyah/belajar_php/echo.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h2>Belajar PHP itu mudah</h2>";
+echo("Hello worls!<br>");
+echo "Aku sedang belajar PHP!<br>";
+echo "ini ", "teks ", "yang ", "dibuat ", "terpisah.";
---
+++ E41192330_Ilmi_Azmi_Iradiyah/belajar_php/tipe_data.php
@@ -0,0 +1,27 @@
+<?php
+
+//tipe data char (karakter)
+$jenis_kelamin = 'P';
+
+//tipe data string (teks)
+$nama_lengkap = "Ilmi Azmi Iradiyah";
+
+//tipe data integer
+$umur = 20;
+
+//tipe data float
+$berat = 67;
+
+//tipe data float
+$tinggi = 163;
+
+//tipe data boolean
+$menikah = "false";
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "Berat Badan: $berat kg<br>";
+echo "Tinggi: $tinggi cm<br>";
+echo "Menikah: $menikah";
+?>
