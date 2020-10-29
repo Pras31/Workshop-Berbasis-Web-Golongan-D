f372a960a05ac1021a8c95bb0197269b1c5ea80f
MohamadArdias <71806525+MohamadArdias@users.noreply.github.com>
2020-10-29 14:20:56 +0700

    Add files via upload

---
+++ E41192114_MOH ARDIAS ADE AGA_form html/belajar_php/constanta.php
@@ -0,0 +1,12 @@
+<?php
+// membuat konstanta
+define('VERSION', '1.0.0');
+
+const SITE_NAME = "Petanikode";
+const BASE_URL  = "https://www.pwtanikode.com" ;
+
+// cetak nilai konstanta
+echo "site name: ". SITE_NAME . "<br>";
+echo "URL: " . BASE_URL . "<br>";
+echo "Version: " . VERSION . "<br>";
+?>
